6134b4a68a5970c3d55ce080d4239fc1dd409460
vijaybhaskar90s <baskar.vijay49@gmail.com>
2026-05-26 10:15:26 +0530

    conversion

--- conversation.sh
+++ conversation.sh
@@ -6,4 +6,4 @@ person3=israel
 echo "$person1: hey iran "
 echo "$person2: hi USA we are ready"
 echo "$person3 : hi trumph please make peace on countries"
-echo "$person1: hi we are in discussion with $person2
+echo "$person1: hi we are in discussion with $person2"
