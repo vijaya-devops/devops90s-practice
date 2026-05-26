159349c911b0bf100ddc814b6e6a317149848359
vijaybhaskar90s <baskar.vijay49@gmail.com>
2026-05-26 10:20:25 +0530

    conversion

---
+++ variables.sh
@@ -0,0 +1,9 @@
+#1 /bin/bash
+
+person1=$1
+person2=$2
+
+echo "$person1: hey iran "
+echo "$person2: hi USA we are ready"
+echo "$person3 : hi trumph please make peace on countries"
+echo "$person1: hi we are in discussion with $person2"
