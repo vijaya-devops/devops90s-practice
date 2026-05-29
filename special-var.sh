2bf3dffb7acdc30e965a934128574dcca3088c00
vijaybhaskar90s <baskar.vijay49@gmail.com>
2026-05-29 21:59:03 +0530

    variables script

---
+++ special-var.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+###special##var chars###
+
+echo "All varaiables  passed to script: $@ "
+echo "first character: $1 "
+echo "Who is running this: $USER"
+echo "Password is: $PWD"
+echo "Home dir: $HOME"
+echo "PI present working dir: $$"
