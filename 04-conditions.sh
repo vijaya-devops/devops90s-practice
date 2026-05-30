432db7a190b6bace02d0a0f048d7685c6dd6dddd
vijaybhaskar90s <baskar.vijay49@gmail.com>
2026-05-30 07:08:15 +0530

    shell practice

--- 04-conditions.sh
+++ 04-conditions.sh
@@ -10,8 +10,8 @@ NUMBER=$1
 #eq equal
 #ne not eqal
 
-if [ $NUMBER -ge 10 ]; then
-echo "given number $NUMBER gerater than 10"
+ if [$NUMBER -ge 20]; then
+echo "given number $NUMBER gerater than 20"
   else 
-  echo "given number $NUMBER less than 10"
+  echo "given number $NUMBER less than 20"
 fi
