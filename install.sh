4ec38bb58b858fcfcac41b0b9d49f2e98ef81b98
vijaybhaskar90s <baskar.vijay49@gmail.com>
2026-05-30 07:25:01 +0530

    shell practice

--- install.sh
+++ install.sh
@@ -5,4 +5,6 @@ if [ $USERID -ne 0 ]; then
 echo "Please run this user with root access"
 exit 1
 fi
-echo "I am continuing"
+#echo "I am continuing"
+echo "Installing mysql"
+dnf install mysqldferew -y
