d2554624f0f8bb425c2c797218969417a1301dc6
vijaybhaskar90s <baskar.vijay49@gmail.com>
2026-05-30 06:59:42 +0530

    shell practice

---
+++ 04-conditions.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+##conditions
+NUMBER=$1
+
+#ge greater than equal
+#le less than equal
+#gt greater than
+#lt less than
+#eq equal
+#ne not eqal
+
+if[$NUMBER is ge 10]
+echo "given number $NUMBER gerater than 10"
+  else 
+  echo "given number $NUMBER less than 10"
+fi
