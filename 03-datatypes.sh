23325ee971c10e4eb65ff114a149947c8bc3f3fc
vijaybhaskar90s <baskar.vijay49@gmail.com>
2026-05-30 06:43:20 +0530

    shell practice

--- 03-datatypes.sh
+++ 03-datatypes.sh
@@ -4,3 +4,9 @@ NUM1=10
 NUM2=20
 SUM=$(($NUM1+$NUM2))
 echo "SUM is: $SUM"
+
+##Arrays
+MOVIES= {"rowdy","Ratak","Kahmirfiles"}
+echo "Movies are: ${MOVIES[$@]}"
+echo "First Movie: ${MOVIES[0]}"
+echo "Second MOvie: ${MOVIES[1]}"
